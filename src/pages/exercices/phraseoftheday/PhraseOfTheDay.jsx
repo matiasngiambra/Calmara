cdedd49562033259ca73544b3e5b49e35b1f95ec
Nahuel Palacios <Nahuel>
2023-11-22 17:33:41 -0300

    Add button to generate new

--- src/pages/exercices/phraseoftheday/PhraseOfTheDay.jsx
+++ src/pages/exercices/phraseoftheday/PhraseOfTheDay.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import '../phraseoftheday/PhraseOfTheDay.css'
 import getRandomQuote from '../../../services/getRandomQuote';
 import CircleLayout from '../../../layout/components/CircleLayout';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Button } from 'react-bootstrap'; // Importa Button desde react-bootstrap
 import FooterComponent from '../../../layout/components/footer/FooterComponent';
 import Navclass from '../../../layout/components/nav/Nav';
 
@@ -23,28 +24,37 @@ export const PhraseOfTheDay = () => {
       setLoading(false);
     }
   };
+
   useEffect(() => {
     fetchQuote();
   }, []);
+
+  const handleGenerateNewQuote = () => {
+    fetchQuote();
+  };
 
   return (
     <div>
       <CircleLayout />
       <Navclass isSecondary={true} />
       <Container className="vh-100 d-flex justify-content-center align-items-center">
-        <Row>
-          <Col>
+        <Row className="d-flex align-items-center">
+          <Col className="d-flex flex-column align-items-center">
             {!loading ? (
               <>
-                <h1 className="text-center">{phraseText}</h1>
-                <p className="text-center text-muted">{phraseAuthor}</p>
+                <h1 className="text-center phrase-text">{phraseText}</h1>
+                <p className="text-center text-muted phrase-autor">{phraseAuthor}</p>
+                <div className='phrase-button-container'>
+                  <Button onClick={handleGenerateNewQuote}>¡Generar nueva frase!</Button> {/* Botón para generar nueva frase */}
+                </div>
               </>
-            ) : <div>Cargando</div>}
+            ) : (
+              <div>Cargando</div>
+            )}
           </Col>
         </Row>
       </Container>
-      <FooterComponent/>
-
+      <FooterComponent />
     </div>
   );
 };
