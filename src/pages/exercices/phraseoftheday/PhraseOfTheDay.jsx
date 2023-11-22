ab4400cffa382c37218c0ebf52cb73a7c4fd0b2d
Nahuel Palacios <Nahuel>
2023-11-22 14:44:37 -0300

    Phrase del dia

--- src/pages/exercices/phraseoftheday/PhraseOfTheDay.jsx
+++ src/pages/exercices/phraseoftheday/PhraseOfTheDay.jsx
@@ -1,31 +1,47 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import getRandomQuote from '../../../services/getRandomQuote';
+import CircleLayout from '../../../layout/components/CircleLayout';
+import { Col, Container, Row } from 'react-bootstrap';
 
 export const PhraseOfTheDay = () => {
+  const [loading, setLoading] = useState(true);
+  const [phraseText, setPhraseText] = useState('');
+  const [phraseAuthor, setPhraseAuthor] = useState('');
 
-    const [loading, setLoading] = useState(true);
-    const [phrase, setPhrase] = useState('');
-  
-    useEffect(() => {
-      const fetchQuote = async () => {
-        try {
-          setLoading(true);
-          const randomQuote = await getRandomQuote();
-          setPhrase(randomQuote.frase);
-        } catch (error) {
-          console.error('Error fetching quote:', error);
-        } finally {
-          setLoading(false); // Ensure loading state is set to false regardless of success/failure
-        }
-      };
-  
-      fetchQuote();
-    }, []);
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        setLoading(true);
+        const randomQuote = await getRandomQuote();
+        const [text, author] = randomQuote.frase.split(' - '); // Dividir la frase en texto y autor
+        setPhraseText(text);
+        setPhraseAuthor(author);
+      } catch (error) {
+        console.error('Error fetching quote:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
+    fetchQuote();
+  }, []);
 
   return (
     <div>
-        {!loading && phrase}
+      <CircleLayout />
+
+      <Container className="vh-100 d-flex justify-content-center align-items-center">
+        <Row>
+          <Col>
+            {!loading && (
+              <>
+                <h1 className="text-center">{phraseText}</h1>
+                <p className="text-center text-muted">{phraseAuthor}</p>
+              </>
+            )}
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
